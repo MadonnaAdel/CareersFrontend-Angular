4fcd800959fc21c97a724993f3fe3305c24bf354
MadonnaAdel <madonnadel0@gmail.com>
2025-05-02 02:30:11 +0300

    Remove ngx-sharebuttons to fix build

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: false,
-  githubClientId: "57f2b47117dcdeddcd33",
-  githubRedirectUri: "http://localhost:4200/register",
-  baseUrl: "http://localhost:3001",
+  baseUrl: "http://careers-api-six.vercel.app/",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   githubClientId: "57f2b47117dcdeddcd33",
   githubRedirectUri: "http://localhost:4200/register",
-  baseUrl: "https://deploying-backend-taupe.vercel.app",
+  baseUrl: "https://careers-api-six.vercel.app/",
 };
